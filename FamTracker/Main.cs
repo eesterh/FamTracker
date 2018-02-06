9c152292eed68b04b1656cb2521329b53b0baddf
eesterh <eesterh007@yahoo.com>
2018-02-06 08:01:56 -0600

    Initial submission of Fam Tracker - empty app with a simple window

---
+++ FamTracker/Main.cs
@@ -0,0 +1,17 @@
+using AppKit;
+using System;
+using System.Drawing;
+using Foundation;
+using ObjCRuntime;
+
+namespace FamTracker
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
