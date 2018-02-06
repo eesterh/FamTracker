589b14ced30184aa1d9d2cee08aab5d56926b082
eesterh <eesterh007@yahoo.com>
2018-02-06 10:30:52 -0600

    Timer example in ViewController with outlet's and action.

--- FamTracker/ViewController.cs
+++ FamTracker/ViewController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 
 using AppKit;
 using Foundation;
@@ -7,6 +8,9 @@ namespace FamTracker
 {
     public partial class ViewController : NSViewController
     {
+        Timer MainTimer;
+        int TimeLeft = 1500;
+
         public ViewController(IntPtr handle) : base(handle)
         {
         }
@@ -16,6 +20,39 @@ namespace FamTracker
             base.ViewDidLoad();
 
             // Do any additional setup after loading the view.
+
+            // Fire the timer once a second
+            MainTimer = new Timer(1000);
+            MainTimer.Elapsed += (sender, e) => {
+                TimeLeft--;
+                // Format the remaining time nicely for the label
+                TimeSpan time = TimeSpan.FromSeconds(TimeLeft);
+                string timeString = time.ToString(@"mm\:ss");
+                InvokeOnMainThread(() => {
+                    //We want to interact with the UI from a different thread,
+                    // so we must invoke this change on the main thread
+                    TimerLabel.StringValue = timeString;
+                });
+
+                // If 25 minutes have passed
+                if (TimeLeft == 0)
+                {
+                    // Stop the timer and reset
+                    MainTimer.Stop();
+                    TimeLeft = 1500;
+                    InvokeOnMainThread(() => {
+                        // Reset the UI
+                        TimerLabel.StringValue = "25:00";
+                        StartStopButton.Title = "Start";
+                        NSAlert alert = new NSAlert();
+                        // Set the style and message text
+                        alert.AlertStyle = NSAlertStyle.Informational;
+                        alert.MessageText = "25 Minutes elapsed! Take a 5 minute break.";
+                        // Display the NSAlert from the current view
+                        alert.BeginSheet(View.Window);
+                    });
+                }
+            };
         }
 
         public override NSObject RepresentedObject
@@ -30,5 +67,22 @@ namespace FamTracker
                 // Update the view, if already loaded.
             }
         }
+
+        partial void StartStopButtonClicked(NSObject sender)
+        {
+            // If the timer is running, we want to stop it,
+            // otherwise we want to start it
+            if (MainTimer.Enabled)
+            {
+                MainTimer.Stop();
+                StartStopButton.Title = "Start";
+            }
+            else
+            {
+                MainTimer.Start();
+                StartStopButton.Title = "Stop";
+            }
+        }
+
     }
 }
--- FamTracker/ViewController.designer.cs
+++ FamTracker/ViewController.designer.cs
@@ -1,18 +1,37 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
+// This file has been generated automatically by Visual Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace FamTracker
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        AppKit.NSButton StartStopButton { get; set; }
+
+        [Outlet]
+        AppKit.NSTextField TimerLabel { get; set; }
+
+        [Action ("StartStopButtonClicked:")]
+        partial void StartStopButtonClicked (Foundation.NSObject sender);
+        
+        void ReleaseDesignerOutlets ()
         {
+            if (TimerLabel != null) {
+                TimerLabel.Dispose ();
+                TimerLabel = null;
+            }
+
+            if (StartStopButton != null) {
+                StartStopButton.Dispose ();
+                StartStopButton = null;
+            }
         }
     }
 }
